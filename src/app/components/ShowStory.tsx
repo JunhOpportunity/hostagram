2e575c07556f299950baa8615168cba9330f7b7b
Junho Kim <twinjyjh5@gmail.com>
2023-05-14 04:57:13 +0000

    Add ShowStory Component Type Of StoryData

--- src/app/components/ShowStory.tsx
+++ src/app/components/ShowStory.tsx
@@ -1,12 +1,17 @@
 import UserImage from "./UserImage";
 
+export type StoryData = {
+  userImageUrl: string;
+  userName: string;
+};
+
 type StoryList = {
-  storyData: string[];
+  storyData: StoryData[];
 };
 
 export default function ShowStory({ storyData }: StoryList) {
   return (
-    <section className="w-full h-[100px] shadow-md gap-4">
+    <section className="flex p-2 w-full h-[100px] shadow-md gap-4 overflow-y-auto">
       {storyData.map((story) => (
         <div className="flex flex-col justfy-center items-center">
           <UserImage userImageUrl={story.userImageUrl} />
