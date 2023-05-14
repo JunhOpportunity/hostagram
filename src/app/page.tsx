3ae05e51d58a39fbb77cb8c71ff6a16c22e41282
Junho Kim <twinjyjh5@gmail.com>
2023-05-14 04:57:40 +0000

    Add AfterLogin About Hompage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,12 @@
-import Image from 'next/image'
-import BeforeLogin from './components/BeforeLogin'
+import Image from "next/image";
+import AfterLogin from "./components/AfterLogin";
+import BeforeLogin from "./components/BeforeLogin";
 
 export default function Home() {
   return (
-    <><BeforeLogin/></>
-  )
+    <>
+      <BeforeLogin />
+      <AfterLogin />
+    </>
+  );
 }
