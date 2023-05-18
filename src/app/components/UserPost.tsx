19c0042962ec40ca44ca7a3801161ab0d17382e1
Junho Kim <twinjyjh5@gmail.com>
2023-05-18 05:23:30 +0000

    style : update Post Category in User Page

--- src/app/components/UserPost.tsx
+++ src/app/components/UserPost.tsx
@@ -16,9 +16,9 @@ export default function UserPosts({ postList }: PostListType) {
   };
   return (
     <section>
-      <div className="flex justify-evenly my-[20px] border-t-4 border-gray-400">
+      <div className="flex justify-evenly p-4 my-[20px] border-t-2 border-gray-200">
         <div
-          className={`flex cursor-pointer ${
+          className={`flex cursor-pointer items-center ${
             category === "posts" && "font-black"
           }`}
           onClick={() => onClick("posts")}
@@ -27,7 +27,7 @@ export default function UserPosts({ postList }: PostListType) {
           POSTS
         </div>
         <div
-          className={`flex cursor-pointer ${
+          className={`flex cursor-pointer items-center ${
             category === "saved" && "font-black"
           }`}
           onClick={() => onClick("saved")}
@@ -36,7 +36,7 @@ export default function UserPosts({ postList }: PostListType) {
           SAVED
         </div>
         <div
-          className={`flex cursor-pointer ${
+          className={`flex cursor-pointer items-center ${
             category === "likes" && "font-black"
           }`}
           onClick={() => onClick("likes")}
