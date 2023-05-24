79f6fe68b56d5fb5e96a1472e98cfdb8972b84d2
Junho Kim <twinjyjh5@gmail.com>
2023-05-24 10:33:57 +0000

    [style] : Update before login - login button style

--- src/app/components/BeforeLogin.tsx
+++ src/app/components/BeforeLogin.tsx
@@ -1,8 +1,10 @@
 export default function BeforeLogin() {
   return (
     <section className="h-80 flex justify-center items-center">
-      <div className="border-2 rounded-md border-red-400 p-1 text-sm font-black hover:bg-gray-400 hover:cursor-pointer">
-        Sign in With Google
+      <div className="rounded-lg bg-gradient-to-r from-pink-500 via-red-500 to-yellow-500 p-[3px] text-sm font-black hover:bg-gray-400 hover:cursor-pointer">
+        <div className="rounded-md bg-white p-[5px] hover:bg-pink-100">
+          Sign in With Google
+        </div>
       </div>
     </section>
   );
