91494e55c5a7d9445b9d6331fa393aa234b5a8e8
Junho Kim <twinjyjh5@gmail.com>
2023-06-01 11:25:57 +0000

    [feat] : Create user icon in header

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -11,6 +11,8 @@ import {
 } from "./ui/icons";
 import { usePathname } from "next/navigation";
 import ColorButton from "./ui/ColorButton";
+import { useSession, signIn, signOut } from "next-auth/react";
+import Image from "next/image";
 
 const menu = [
   {
@@ -32,6 +34,8 @@ const menu = [
 
 export default function Header() {
   const pathname = usePathname();
+  const { data: session } = useSession();
+  session && console.log(session?.user);
   return (
     <header className="flex justify-between p-2 w-full max-w-screen-xl mx-auto items-center">
       <Link href="/" className="font-black text-2xl">
@@ -47,7 +51,25 @@ export default function Header() {
             {pathname == item.href ? item.clickedIcon : item.icon}
           </Link>
         ))}
-        <ColorButton text={"Sing in"} onClick={() => {}} />
+        {session && (
+          <Link
+            href={`/user/${session.user?.name}`}
+            className="w-[25px] h-[25px] relative"
+          >
+            <Image
+              className="rounded-full border-2"
+              src={`${session.user?.image}`}
+              fill={true}
+              style={{ objectFit: "cover" }}
+              alt="User Image"
+            />
+          </Link>
+        )}
+        {session ? (
+          <ColorButton text={"Sign out"} onClick={() => signOut()} />
+        ) : (
+          <ColorButton text={"Sign in"} onClick={() => signIn("google")} />
+        )}
       </nav>
     </header>
   );
