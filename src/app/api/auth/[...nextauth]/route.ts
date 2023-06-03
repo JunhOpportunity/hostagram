83bb0839dd2ef695c49fb1ec8b491340c7d06b5e
Junho Kim <twinjyjh5@gmail.com>
2023-06-03 07:56:28 +0000

    [feat] : Create auth

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,13 +1,25 @@
-import NextAuth from "next-auth";
+import NextAuth, { NextAuthOptions } from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
 
-export const handler = NextAuth({
+export const handler: NextAuthOptions = NextAuth({
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_OAUTH_ID || "",
       clientSecret: process.env.GOOGLE_OAUTH_SECRET || "",
     }),
   ],
+  callbacks: {
+    session({ session }) {
+      const user = session?.user;
+      if(user) {
+        session.user = {
+          ...user,
+          username: user.email?.split('@')[0] || '',
+        }
+      }
+      return session;
+    },
+  },
   pages: {
     signIn: "/auth/signin",
   },
