55b1a0f38ca935a228c4efea48015f65995d9a5b
Junho Kim <twinjyjh5@gmail.com>
2023-06-05 11:37:36 +0000

    [style] : Update avatar style

--- src/app/components/Avatar.tsx
+++ src/app/components/Avatar.tsx
@@ -2,11 +2,11 @@ import Image from "next/image";
 
 export default function Avatar({ image }: { image?: string | null }) {
   return (
-    <div className="w-[25px] h-[25px] rounded-lg bg-gradient-to-r from-pink-500 via-red-500 to-yellow-500 text-sm font-bold hover:bg-gray-400 hover:cursor-pointer">
+    <div className="w-[25px] h-[25px] rounded-full bg-gradient-to-r from-pink-500 via-red-500 to-yellow-500 text-sm font-bold hover:bg-gray-400 hover:cursor-pointer">
       <img
-        alt="user profil"
+        alt="user profile"
         src={image ?? undefined}
-        className="rounded-full border-2 p-[0.1rem]"
+        className="w-[25px] h-[25px] rounded-full p-[0.1rem] overflow-hidden object-cover"
         referrerPolicy="no-referrer"
       />
     </div>
