052e144340e3596b2021fa2109c8dbf6f463e6f9
Junho Kim <twinjyjh5@gmail.com>
2023-06-16 11:43:11 +0000

    [chore] : Update carousel api response style

--- src/app/components/FollowingBar.tsx
+++ src/app/components/FollowingBar.tsx
@@ -1,19 +1,28 @@
 "use client";
 
-import { useUser } from "@/service/swr";
+import useSWR from "swr";
 import { ClipLoader } from "react-spinners";
+import { DetailUser } from "@/model/user";
 import FollowingCarousel from "./FollowingCarousel";
 
-export default function FollowingBar({email} : {email: string}) {
+export default function FollowingBar({ email }: { email: string }) {
+  const { data, isLoading, error } = useSWR<DetailUser>(`/api/me`);
+  const user = data?.following;
+  console.log("test data", data?.following);
 
-  const {user, isError, isLoading} = useUser(email)
-  console.log(user);
-  if (isError) return <div>failed to load</div>;
-  if (isLoading) return <ClipLoader color="#d63636" />;
+  // const {user, isError, isLoading} = useUser(email)
+  // console.log(user);
 
+  if (error) return <div>failed to load</div>;
   return (
-    <div>
-      <FollowingCarousel following={user.data[0].following}/>
-    </div>
+    <section className="w-full py-[20px] px-[10px] border border-slate-300 rounded-md min-h-[90px] overflow-x-auto">
+      {isLoading ? (
+        <ClipLoader color="#d63636" />
+      ) : !user || user.length == 0 ? (
+        <p>{`You don't have following`}</p>
+      ) : (
+        <FollowingCarousel following={user} />
+      )}
+    </section>
   );
 }
