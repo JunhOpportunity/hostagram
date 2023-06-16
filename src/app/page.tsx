0cf8977d6c8c23a6ecc34834c8a59c19df3a6fb8
Junho Kim <twinjyjh5@gmail.com>
2023-06-16 11:39:57 +0000

    [style] : Update sidebar style

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,12 +2,11 @@ import { getServerSession } from "next-auth";
 import { handler } from "./api/auth/[...nextauth]/route";
 import { redirect } from "next/navigation";
 import SideBar from "./components/SideBar";
-import SwrTest from "./components/FollowingBar";
+import FollowingBar from "./components/FollowingBar";
 
 export default async function HomePage() {
   const session = await getServerSession(handler);
   const user = session?.user;
-  console.log("유저 데이터", user);
 
   if (!user) {
     redirect("/auth/signin");
@@ -17,9 +16,9 @@ export default async function HomePage() {
     <section className="w-full flex flex-col md:flex-row max-w-[850px] p-4 mx-auto gap-10">
       <div className="w-full basis-3/4">
         {/*<ShowPosts />*/}
-        <SwrTest email={user.email} />
+        <FollowingBar email={user.email} />
       </div>
-      <div className="basis-1/4">
+      <div className="basis-1/4 ml-8">
         <SideBar user={user} />
       </div>
     </section>
