46e075321d5e264c3c9736c065ee658110b0a0c7
Junho Kim <twinjyjh5@gmail.com>
2023-06-17 11:22:56 +0000

    [chore] : Update post data name usefully

--- src/service/post.ts
+++ src/service/post.ts
@@ -1,7 +1,8 @@
 import { client } from "./sanity";
 
 export async function getAllPosts() {
-  const query = '*[_type == "post"]{..., "postImageUrl": photo.asset->url, author->, comments[]{..., author->}, likes[]->}';
+  const query =
+    '*[_type == "post"]{..., "postImageUrl": photo.asset->url, author->, comments[]{..., author->}, likes[]->}';
   const allPosts = await client.fetch(query);
   return allPosts;
 }
@@ -10,4 +11,24 @@ export async function getPosts() {
   const query = '*[_type == "post"]';
   const data = await client.fetch(query);
   return;
+}
+
+const simplePostPorjection = `
+  ...,
+  "username": author->username,
+  "userImage": author->image,
+  "image": photo,
+  "likes": likes[]->username,
+  "text": comments[0].comment,
+  "comments": count(comments)",
+  "id":_id,
+  "createdAt":_createdAt
+`
+
+export async function getFollowingPosts(username: string) {
+  const query = `*[_type == "post" && author->username == "${username}"
+   || author._ref in *[_type=="user" && username == "${username}"].following[]._ref ]
+    | order(_createdAt desc){${simplePostPorjection}}`;
+  const data = await client.fetch(query);
+  return data;
 }
