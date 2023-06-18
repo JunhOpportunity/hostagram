9a636f04426e945eea904529b333cafceb8ff5e6
Junho Kim <twinjyjh5@gmail.com>
2023-06-18 08:27:26 +0000

    [chore] : Add domains in next config

--- next.config.js
+++ next.config.js
@@ -1,10 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {images: {
+const nextConfig = {
+  images: {
+    domains: ['cdn.sanity.io'],
+
   remotePatterns: [
     {
       protocol: 'https',
       hostname: 'lh3.googleusercontent.com',
     },
+    
   ],
 },}
 
