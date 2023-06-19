52d89b02fc5fad94954804ca1c9aac152cf45a65
Junho Kim <twinjyjh5@gmail.com>
2023-06-19 07:37:26 +0000

    [feat] : Add getPost for detail post data

--- src/service/post.ts
+++ src/service/post.ts
@@ -37,3 +37,20 @@ export async function getFollowingPosts(email: string) {
       posts.map((post: SimplePost) => ({ ...post, image: urlFor(post.image) }))
     );
 }
+
+export async function getPost(id: string) {
+  return client.fetch(
+    `*[_type == "post" && _id == "${id}"][0]{
+      ...,
+      "username": author->username,
+      "userImage": author->image,
+      "image":photo,
+      "likes": likes[]->username,
+      comments[]{comment, "username": author->username, "image": author->image},
+      "id":_id,
+      "createdAt":_createdAt
+    }`
+  ).then((posts) =>
+  posts.map((post: SimplePost) => ({ ...post, image: urlFor(post.image) }))
+);
+}
