95076d2c1f5cb71e95a25601792fc256cb016061
Junho Kim <twinjyjh5@gmail.com>
2023-06-24 06:21:33 +0000

    [feat] : Create function of like & dislike post

--- src/service/post.ts
+++ src/service/post.ts
@@ -95,3 +95,18 @@ export async function getSavedPostsOf(username: string) {
       posts.map((post: SimplePost) => ({ ...post, image: urlFor(post.image) }))
     );
 }
+
+export async function likePost(postId: string, userId: string) {
+  return client
+    .patch(postId)
+    .setIfMissing({ likes: [] })
+    .append("likes", [{ _ref: userId, _type: "reference" }])
+    .commit({ autoGenerateArrayKeys: true });
+}
+
+export async function dislikePost(postId: string, userId: string) {
+  return client
+    .patch(postId)
+    .unset([`likes[_ref=="${userId}]`])
+    .commit();
+}
